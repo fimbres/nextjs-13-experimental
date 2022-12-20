85b11057ede07e155bc7471992e80a7dd9027c18
Isaac <fimbresisaac23@gmail.com>
2022-12-20 01:24:34 -0800

    working with the new Image component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
 
 import styles from '../styles/home.module.css'
 
 const HomePage = () => {
   return (
     <div className={styles.page}>
-      <div className={styles.title}>Home Page</div>
+      <div className={styles.container}>
+        <Image src='/../public/logo.png' alt='next-logo' width={200} height={200} />
+        <div className={styles.title}>Home Page</div>
+      </div>
     </div>
   )
 }
